afc1598b6c3286cb18250784212bceb2843c9f11
srinathlaka <srinathlaka1@gmail.com>
2023-09-25 18:17:48 +0200

    Update of excepetion file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 
 from flask import Flask , render_template
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 app = Flask(__name__, static_folder="static")
 
 
 @app.route("/",methods = ['GET','POST'])
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("We are testing custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return render_template("index.html")
 
 
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -4,31 +4,31 @@ import sys
 
 class HousingException(Exception):
 
-    def  __init__(self , error_message:Exception,error_details:sys):
+    def  __init__(self , error_message:Exception,error_detail:sys):
         super().__init__(error_message)
         self.error_message = HousingException.get_detailed_error_message(error_message=error_message,
-                                                                         error_details=error_details)
+                                                                         error_detail=error_detail)
 
 
     @staticmethod
-    def get_detailed_error_message(error_message:Exception, error_detail:sys)->str:
-
+    def get_detailed_error_message(error_message: Exception, error_detail: sys) -> str:
         """
-        
         error_message: Exception object
-        error_detail: ibject of sys module
+        error_detail: object of sys module
         """
 
-        _,_exec_tb = error_detail.exc_info()
-        line_number = _exec_tb.tb_frame.f_lineno
-        file_name = _exec_tb.tb_frame.f_code.co_filename
+        exc_type, exc_value, exc_traceback = error_detail.exc_info()
+        line_number = exc_traceback.tb_lineno
+        file_name = exc_traceback.tb_frame.f_code.co_filename
 
-        error_message = f"Error occured in script; [{file_name}] at line munber: [{line_number}] error message : [{error_message}]"
-
-
+        error_message = f"Error occurred in script [{file_name}] at line number [{line_number}]: {error_message}"
 
         return error_message
     
 
     def __str__(self):
         return self.error_message
+    
+
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
